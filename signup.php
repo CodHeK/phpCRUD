5547ce5b5487e6b343e76b02ff45a2b4c3a338c4
Gagan Ganapathy <gaganganapathyas@yahoo.in>
2017-05-23 20:18:42 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -3,10 +3,19 @@
 <head>
 	<title>ThankYou</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link href="https://fonts.googleapis.com/css?family=Changa:200|Source+Sans+Pro:200" rel="stylesheet">
+	<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.4.js"></script>
+
+	<style type="text/css">
+		body {
+			background-color: black;
+			color: white;
+		}
+	</style>
 </head>
 <body>
 <?php
-	$name = $_POST['name'];
+	$name = $_POST['firstname'];
 	$email = $_POST['email'];
 	$password = $_POST['password'];
 	$rpassword = $_POST['rpassword'];
@@ -23,35 +32,38 @@
 				$result = mysqli_query($dbc, $query)
 					or die('Error connecting to DataBase2');
 
-				$message = "
-					<html>
-					<body>
-						<h1>Please Click on the link below to verify your email ID</h1>
-						<a href='http://localhost/phpLogin/login.html'>http://localhost/phpLogin/login.html</a>
-					</body>
-					</html>
-				";
+				// $message = "
+				// 	<html>
+				// 	<body>
+				// 		<h1>Please Click on the link below to verify your email ID</h1>
+				// 		<a href='http://localhost/phpLogin/login.html'>http://localhost/phpLogin/login.html</a>
+				// 	</body>
+				// 	</html>
+				// ";
 
-				$from = "iit2016038@iiita.ac.in";
+				// $from = "iit2016038@iiita.ac.in";
 
-				$to = $email;
+				// $to = $email;
 
-				$subject = "Verification Mail";
+				// $subject = "Verification Mail";
 
-				$mail = mail($to,$subject,$message,'From:'.$from);
+				// $mail = mail($to,$subject,$message,'From:'.$from);
 
-				if($mail) {
-					echo 'Mail Sent';
-				}
-				else {
-					echo 'Mail not Sent';
-				}
+				// if($mail) {
+				// 	echo 'Mail Sent';
+				// }
+				// else {
+				// 	echo 'Mail not Sent';
+				// }
 
 				mysqli_close($dbc);
 
 				echo '<div class="container">', '<br>';
-				echo '<h1>ThankYou " ', $name,' " For Submitting Form</h1>', '<br>';
-				echo '<h2> Please, Check your mail and verify it ! :D </h2>', '<br>';
+				echo '<div id="content" style="text-align:center;margin: 0 auto;font-family:Source Sans Pro;">';
+				echo '<h3>ThankYou " ', $name,' " For Submitting Form</h3>', '<br>';
+				echo '<h4> Please, Login to Continue</h4>', '<br>';
+				echo '<a href="http://localhost/phpcrud/index.html#" class="btn btn-deafult">LOG IN</a>', '</br>';
+				echo '</div>';
 				echo '</div>';
 
 			}
